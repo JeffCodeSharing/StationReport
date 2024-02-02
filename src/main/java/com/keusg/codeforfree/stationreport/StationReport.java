27c66fbae936288ea0b59b215fc2bff68da6d5cf
AIR <3545210791@qq.com>
2024-02-02 14:28:38 +0800

    init

--- src/main/java/com/keusg/codeforfree/stationreport/StationReport.java
+++ src/main/java/com/keusg/codeforfree/stationreport/StationReport.java
@@ -1,2 +1,13 @@
-package com.keusg.codeforfree.stationreport;public class StationReport {
+package com.keusg.codeforfree.stationreport;
+
+import net.fabricmc.api.ModInitializer;
+
+public class StationReport implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
 }
--- src/main/java/com/keusg/codeforfree/stationreport/client/StationReportClient.java
+++ src/main/java/com/keusg/codeforfree/stationreport/client/StationReportClient.java
@@ -1,2 +1,13 @@
-package com.keusg.codeforfree.stationreport.client;public class StationReportClient {
+package com.keusg.codeforfree.stationreport.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class StationReportClient implements ClientModInitializer {
+    /**
+     * Runs the mod initializer on the client environment.
+     */
+    @Override
+    public void onInitializeClient() {
+
+    }
 }
